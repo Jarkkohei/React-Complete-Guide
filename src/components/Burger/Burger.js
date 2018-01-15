d689777fdbe515d924ae683ab0e8a92464bdd4ac
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-15 18:02:39 +0200

    Added outputting the Burger Ingredients dynamically. [113]

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    
+    //  Get the ingredients names (keys) from the object...
+    const transformedIngredients = Object.keys(props.ingredients)
+        //  Map through every name (igKey)...
+        .map(igKey => {
+            //  Return an array of ingredient names (igKey) as keys and make a "key"-prop for each them with the name and index (Ex. Salad1, Salad2,...).
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
