36f0a47af1ffed10751c239f1f86fcb27897c28e
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-16 15:42:17 +0200

    Added the rendering multiple "BuildControl"-components. Also changed "Less"- and "More"-buttons to "-" and "+"

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,8 @@ import classes from './BuildControl.css';
 const buildControl = ( props ) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.Less}>-</button>
+        <button className={classes.More}>+</button>
     </div>
 );
 
