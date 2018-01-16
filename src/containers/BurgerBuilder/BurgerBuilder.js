24081829ff53dd939a7cb0172f72420aeb7dd56e
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-16 18:55:07 +0200

    Added the "addIngredientHandler"-method to the BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from './../../components/Burger/Burger';
 import BuildControls from './../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 
     /* An alternative way of implementing the state.
@@ -19,8 +26,35 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        //  Update the count of an ingredient.
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+
+        //  Fetch the ingredients object from the state and update the count.
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+
+        //  Fetch the ingredient price from the INGREDIENT_PRICES.
+        const priceAddition = INGREDIENT_PRICES[type];
+        //  Fetch the totalPrice from the state.
+        const oldPrice = this.state.totalPrice;
+        //  Add the price of the ingredient to totalPrice.
+        const newPrice = oldPrice + priceAddition;
+        //  Set the state accordingly.
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
+
 
     render() {
         return(
