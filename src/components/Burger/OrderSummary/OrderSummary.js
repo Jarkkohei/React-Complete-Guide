7aed370d89e0678e6c280786872a38b54b5b77ec
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-17 18:53:30 +0200

    Added the Price to the OrderSummary. [126]

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -26,6 +26,7 @@ const orderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
+            <p>Total Price: <strong>{props.orderPrice.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
