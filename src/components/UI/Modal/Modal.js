953335ed9f75d9034b28402adf7dc4995b3e922a
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-19 13:46:54 +0200

    Added the "shouldComponentUpdate"-lifecyclehook-method to the "Modal"-component, to check if it really needs to be updated.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,6 +6,14 @@ import Backdrop from './../Backdrop/Backdrop';
 
 class Modal extends Component {
 
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] will update');
+    }
+
     render() {
 
         return(
