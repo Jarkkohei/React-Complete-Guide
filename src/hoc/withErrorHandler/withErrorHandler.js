85e86d432eed049b1dbac72da05df755424195cd
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-22 13:45:52 +0200

    Changed the "withErroroHandler"-hoc from being a functional-component, to being a class-based-component. Also added the error handling in the "componentDidMount"-method.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from './../../components/UI/Modal/Modal';
 import Aux from './../Auxiliary/Auxiliary';
 
-const withErrorHandler = (WrappedComponent) => {
-    return(props) => {
-        return(
-            <Aux>
-                <Modal>
-                    Something didn't work right!
-                </Modal>
-                <WrappedComponent {...props} />
-            </Aux>
-        );
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            //  Clear the possible existing errors.
+            axios.interceptors.request.use(req => {
+                this.setState({errors: null});
+            });
+            //  Set the state.error to the actual response error-object (including the message.property).
+            axios.interceptors.response.use(null, error => {
+                this.setState({error: error});
+            });
+        }
+
+        render() {
+            return(
+                <Aux>
+                    <Modal show={this.state.error}>
+                        {this.state.error.message}}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }  
     }
 }
 
