deea118ec29c7e95762095f8a9f909b987567d12
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-24 19:54:39 +0200

    Changed the storing the order to the Firebase in "BurgerBuilder.purchaseContinueHandler()"-method, to just navigating to the "/checkout"-route. [205]

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -122,32 +122,33 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
-        this.setState({loading: true});
+        // this.setState({loading: true});
 
-        //  !!! You should NOT calculate the price at the client-side.
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Pekka Virtanen',
-                address: {
-                    street: 'Testikatu 1',
-                    zipCode: '001155',
-                    country: 'Finland'
-                },
-                email: 'texst@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // //  !!! You should NOT calculate the price at the client-side.
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Pekka Virtanen',
+        //         address: {
+        //             street: 'Testikatu 1',
+        //             zipCode: '001155',
+        //             country: 'Finland'
+        //         },
+        //         email: 'texst@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        //  ".json" id required by the Firebase
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // //  ".json" id required by the Firebase
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     }
 
 
