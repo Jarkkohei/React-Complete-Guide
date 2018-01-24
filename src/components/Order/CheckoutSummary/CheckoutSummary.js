3f45a1ab5eb84331f90e6dc0a107f0a96cf47cd0
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-24 18:03:34 +0200

    Created the "Checkout"-container and the "CheckoutSummary"-component.

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const checkoutSummary = (props) => {
+    
+};
+
+export default checkoutSummary;
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+
+    state = {
+        
+    }
+
+    render() {
+        return(
+            <p>Checkout</p>
+        );
+    }
+
+}
+
+export default Checkout;
