cf0b19cd76be621e406605c5a4047b027ae1ce93
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-25 16:32:21 +0200

    Added the Route for the "Orders"-component into the "App.js"-file.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import Order from './../../components/Order/Order';
+
 class Orders extends Component {
     render() {
-        return ();
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        );
     }
 }
 
