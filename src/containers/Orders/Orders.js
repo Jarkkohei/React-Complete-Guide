3068daf7fcb04b4f4ada1deb5802662a4a110df7
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-01-26 18:18:20 +0200

    Added the fetching of the orders from the Firebase to the "Orders"-component. [212]

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import axios from './../../axios-orders';
+import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
 
 import Order from './../../components/Order/Order';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+
+    componentDidMount() {
+        axios.get('orders.json')
+            .then(res => {
+
+                //  Convert the fetched object into an array.
+                const fetchedOrders = [];
+
+                for(let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+
+                //  Fetching the orders has stopped (successfully).
+                this.setState({loading: false, orders: fetchedOrders});
+            })
+            .catch(err => {
+                //  Fetching the orders has stopped (failed).
+                this.setState({loading: false});
+            });
+    }
+
+
     render() {
         return (
             <div>
@@ -13,4 +46,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
