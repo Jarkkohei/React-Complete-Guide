ab74ec49427c4e54642947deffd6a46600b117ee
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-03-04 15:11:43 +0200

    Added the "redux-thunk" and applied it as a  middleware in the index.js. #285

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,13 +28,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         console.log(this.props);
-        // axios.get('https://react-my-burger-899db.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data});
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true});
-        //     });
+         axios.get('https://react-my-burger-899db.firebaseio.com/ingredients.json')
+             .then(response => {
+                 this.setState({ingredients: response.data});
+             })
+            .catch(error => {
+                 this.setState({error: true});
+             });
     }
 
 
