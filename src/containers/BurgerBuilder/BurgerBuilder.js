7b14268075fa7e9e54591b9797bfffd4ee355f60
Jarkko Heikkinen <jarkkohei@luukku.com>
2018-03-10 09:27:30 +0200

    Added the actions for fetching the ingredients asyncronously. DOES NOT WORK YET! #286

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from './../../axios-orders';
 
 import Aux from './../../hoc/Auxiliary/Auxiliary';
 import Burger from './../../components/Burger/Burger';
@@ -8,7 +9,6 @@ import Modal from './../../components/UI/Modal/Modal';
 import OrderSummary from './../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from './../../components/UI/Spinner/Spinner';
 import withErrorHandler from './../../hoc/withErrorHandler/withErrorHandler';
-import axios from './../../axios-orders';
 import * as burgerBuilderActions from './../../store/actions/index';
 
 class BurgerBuilder extends Component {
@@ -21,20 +21,11 @@ class BurgerBuilder extends Component {
     */
 
     state = {
-        purchasing: false,
-        loading: false,
-        error: false
+        purchasing: false
     }
 
     componentDidMount() {
         console.log(this.props);
-         axios.get('https://react-my-burger-899db.firebaseio.com/ingredients.json')
-             .then(response => {
-                 this.setState({ingredients: response.data});
-             })
-            .catch(error => {
-                 this.setState({error: true});
-             });
     }
 
 
@@ -99,10 +90,6 @@ class BurgerBuilder extends Component {
                 purchaseCancelled={this.purchaseCancelHandler} 
                 purchaseContinued={this.purchaseContinueHandler}
                 orderPrice={this.props.price}/>;
-        }
-
-        if(this.state.loading) {
-            orderSummary = <Spinner />;
         }
 
         return(
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from './../../axios-orders';
 
 export const addIngredient = (name) => {
     return {
@@ -12,5 +13,36 @@ export const removeIngredient = (name) => {
     return {
         type: actionTypes.REMOVE_INGREDIENT,
         ingredientName: name
+    };
+};
+
+
+//  Syncronous action used by asyncronous action initIngredients().
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    };
+};
+
+
+//  Syncronous action used by asyncronous action initIngredients().
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    };
+}
+
+
+//  Asyncronous action using syncronous actions
+export const initIngredients = () => {
+    return dispatch = {
+        axios.get('https://react-my-burger-899db.firebaseio.com/ingredients.json')
+             .then(response => {
+                 dispatchEvent(setIngredients(response.data));
+             })
+            .catch(error => {
+                 dispatch(fetchIngredientsFailed());
+             });
     };
 };
