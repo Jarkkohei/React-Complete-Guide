e0fc73ed9ee782906e55f6f4c10b612278e655dd
Jarkko <jarkkohei@luukku.com>
2018-06-14 19:23:26 +0300

    Refactor the 'NavigationItems'-tests to use 'wrapper.setProps()' to set testable properties. #342

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -13,13 +13,18 @@ configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
 
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
 
     it('should render three <NavigationItem /> elements if authenticated', () => {
-        const wrapper = shallow(<NavigationItems isAuthenticated/>);
+        // wrapper = shallow(<NavigationItems isAuthenticated />);
+        wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 });
