c9495a83c2ae921182be390eed30fe48ad1374ca
Jarkko <jarkkohei@luukku.com>
2019-03-29 10:36:37 +0200

    Add redux-saga middleware and use it instead of calling the actions directly

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,6 +12,7 @@ import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import { watchAuth } from './store/sagas';
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
@@ -20,7 +22,11 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = process.env.NODE_DEV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,12 +25,12 @@ export const authFail = (error) => {
 
 export const logout = () => {
     //  Clear the localStorage
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userId');
 
     return {
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 };
 
