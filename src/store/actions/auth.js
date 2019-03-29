20d3a24330eb4aeab770389af6cc3ec1840e7d4b
Jarkko <jarkkohei@luukku.com>
2019-03-29 11:32:57 +0200

    Handle authentication with saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,33 +48,11 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.REACT_APP_FIREBASE_API_KEY;
-        if(!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.REACT_APP_FIREBASE_API_KEY;
-        }
-        axios.post(url, authData)
-            .then(response => {
-                //  Get the current date and time and calculate the expirationDate.
-                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
-
-                localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId', response.data.localId);
-
-                dispatch(authSuccess(response.data.idToken, response.data.localId));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(error => {
-                dispatch(authFail(error.response.data.error));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
     };
 };
 
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,5 +1,6 @@
 import { delay } from 'redux-saga';
 import { put } from 'redux-saga/effects';
+import axios from 'axios';
 
 import * as actions from '../actions/index';
 
@@ -14,4 +15,34 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
     yield delay(action.expirationTime * 1000);
     yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+    yield pu(actions.authStart());
+    dispatch(authStart());
+
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true
+    };
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.REACT_APP_FIREBASE_API_KEY;
+    if (!action.isSignup) {
+        url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.REACT_APP_FIREBASE_API_KEY;
+    }
+
+    try {
+        const response = yield axios.post(url, authData)
+            //  Get the current date and time and calculate the expirationDate.
+        const expirationDate = yield new Date(new Date().getTime() + response.data.expiresIn * 1000);
+
+        yield localStorage.setItem('token', response.data.idToken);
+        yield localStorage.setItem('expirationDate', expirationDate);
+        yield localStorage.setItem('userId', response.data.localId);
+
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId));
+        yield put(actions.checkAuthTimeout(response.data.expiresIn));
+    } catch (error) {
+        yield put(actions.authFail(error.response.data.error));
+    }
 }
