b2148cebb3bd681d7b18366a559b7b70ec53ce91
Jarkko <jarkkohei@luukku.com>
2019-04-03 20:48:45 +0300

    Fix bugs that caused an infinite loop on Orders-component lo

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -11,7 +11,7 @@ const orders = props => {
 
     useEffect(() => {
         props.onFetchOrders(props.token, props.userId);
-    });
+    }, []);
 
     let orders = <Spinner />;
     if(!props.loading) {
